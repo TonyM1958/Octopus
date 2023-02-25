276dcbf6129289c65d5ea68483ed1a1a27f83bc3
TonyM1958 <tony.matthews1@outlook.com>
2023-02-25 17:11:05 +0000

    added template for private.py

---
+++ template private.py
@@ -0,0 +1,21 @@
+##################################################################################################
+"""
+Module:   Private data
+Date:     23 February 2023
+"""
+##################################################################################################
+# this is a template for the code used to store your private data. Copy this file, add the details
+# and rename the file to 'private.py' so the details can be imported. 'private.py' should be
+# configured so it is ignored by github to stop your private data being published. 
+##################################################################################################
+
+from energy import Meter
+
+# private data
+octopus_api_key = 'sk_live_ ****'                                           # your API key copied from your Octopus account developer settings
+imp_meter = Meter(mpn = '** mpan **', ser = '** serial number **')          # the MPAN and serial number of your import meter
+exp_meter = Meter(mpn = '** mpan **', ser = '** serial number **')          # the MPAN and serial number for export meter (serial may be same import and export, but MPAN is different)
+gas_meter = Meter(mpn = '** mprn **', ser = '** serial number **')          # the MPRN and serial number for your gas meter               
+
+solcast_api_key = '****'      # your API key from the Solcast account
+rids = ['****','****']        # the rids for the Home PV system sites you setup in Solcast. The free tier allows you to setup 2 sites e.g. east and west facing panels
